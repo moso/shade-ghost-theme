c1a7011fc4a304d8e9e7372800d79e2ffeab6193
Morten Sørensen <morten@moso.io>
2017-12-18 22:50:37 +0100

    Added 3up sidebar Sass

--- src/js/shade.js
+++ src/js/shade.js
@@ -1,17 +1,104 @@
 // SIDEBAR
-$(function() {
-    // Open navbarSide when button is clicked
-    $('.navbar-toggler').on('click', function() {
-        $('.navbar-side').addClass('reveal');
-        $('.overlay').show();
-    });
+var window_height;
+var window_width;
+var navbar_initialized = false;
+var nav_toggle;
 
-    // Close navbarSide when the outside of menu is clicked
-    $('.overlay').on('click', function(){
-        $('.navbar-side').removeClass('reveal');
-        $('.overlay').hide();
+var shadeNav = {
+    sidenav: {
+        sidenav_visible: 0
+    },
+    initSideNav: function initSideNav() {
+        if (!navbar_initialized) {
+            var $nav = $('nav');
+            $nav.addClass('navbar-shade');
+
+            var $navbar = $nav.find('.navbar-collapse').first().clone(true);
+            $navbar.css('min-height', window.screen.height);
+
+            var ul_content = '';
+
+            $navbar.children('ul').each(function () {
+                var show_content = $(this).html();
+                ul_content = ul_content + show_content;
+            });
+
+            ul_content = '<ul class="navbar-nav">' + ul_content + '</ul>';
+            $navbar.html(ul_content);
+
+            $('body').append($navbar);
+
+            var $toggle = $('.navbar-toggler');
+
+            $navbar.find('a').removeClass('btn btn-round btn-default');
+            $navbar.find('button').removeClass('btn-round btn-fill btn-info btn-primary btn.success btn-warning btn-danger');
+            $navbar.find('button').addClass('btn-simple btn-block');
+
+            $toggle.on('click', function () {
+                if (shadeNav.sidenav.sidenav_visible == 1) {
+                    $('html').removeClass('nav-open');
+                    shadeNav.sidenav.sidenav_visible = 0;
+                    $('#overlay').remove();
+                    setTimeout(function () {
+                        $toggle.removeClass('toggled');
+                    }, 300);
+                } else {
+                    setTimeout(function () {
+                        $toggle.addClass('toggled');
+                    }, 300);
+
+                    var div = '<div id="overlay"></div>';
+                    $(div).appendTo("body").click(function () {
+                        $('html').removeClass('nav-open');
+                        shadeNav.sidenav.sidenav_visible = 0;
+                        $('#overlay').remove();
+                        setTimeout(function () {
+                            $toggle.removeClass('toggled');
+                        }, 300);
+                    });
+
+                    $('html').addClass('nav-open');
+                    shadeNav.sidenav.sidenav_visible = 1;
+                }
+            });
+            navbar_initialized = true;
+        }
+    }
+};
+
+$(document).ready(function () {
+    window_width = $(window).width();
+    window_height = $(window).height();
+
+    nav_toggle = $('nav').hasClass('navbar-shade') ? true : false;
+
+    if (window_width < 992 || nav_toggle) {
+        shadeNav.initSideNav();
+    }
+
+    $('.nav-link').on('click', function () {
+        var $toggle = $('.navbar-toggler');
+
+        $('html').removeClass('nav-open');
+        shadeNav.sidenav.sidenav_visible = 0;
+        setTimeout(function () {
+            $toggle.removeClass('toggled');
+        }, 300);
     });
 });
+
+$(window).resize(function () {
+    if ($(window).width() < 992) {
+        shadeNav.initSideNav();
+    }
+
+    if ($(window).width() > 992 && !nav_toggle) {
+        $('nav').removeClass('navbar-shade');
+        shadeNav.sidenav.sidenav_visible = 1;
+        navbar_initialized = false;
+    }
+});
+
 
 // INFINITE SCROLL PAGINATION
 $(function() {
